ceb7eb481f4a53833a6869a22815e5627fc7200a
douglas.fonseca <douglas.hfonseca@gmail.com>
2021-02-19 11:46:56 -0300

    'teste3'

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Form from "./Form";
+import Tarefas from "./Tarefas";
 
-//Form
-import { FaPlus } from 'react-icons/fa'
-
-//Tarefas
-import { FaEdit, FaWindowClose } from 'react-icons/fa'
-
-
-import './Main.css';
+import "./Main.css";
 
 export default class Main extends Component {
   state = {
-    novaTarefa: '',
+    novaTarefa: "",
     tarefas: [],
     index: -1,
   };
+
+  componentDidMount() {
+    const tarefas = JSON.parse(localStorage.getItem("tarefas"));
+
+    if (!tarefas) return;
+
+    this.setState({ tarefas });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tarefas } = this.state;
+
+    if (tarefas === prevState.tarefas) return;
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  }
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -29,25 +40,24 @@ export default class Main extends Component {
     if (index === -1) {
       this.setState({
         tarefas: [...novasTarefas, novaTarefa],
-        novaTarefa: '',
-      })
+        novaTarefa: "",
+      });
     } else {
       novasTarefas[index] = novaTarefa;
 
       this.setState({
         tarefas: [...novasTarefas],
         index: -1,
-        novaTarefa: '',
-      })
+        novaTarefa: "",
+      });
     }
-
-  }
+  };
 
   handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value,
     });
-  }
+  };
 
   handleEdit = (e, index) => {
     const { tarefas } = this.state;
@@ -55,8 +65,8 @@ export default class Main extends Component {
     this.setState({
       index,
       novaTarefa: tarefas[index],
-    })
-  }
+    });
+  };
 
   handleDelete = (e, index) => {
     const { tarefas } = this.state;
@@ -65,40 +75,26 @@ export default class Main extends Component {
 
     this.setState({
       tarefas: [...novasTarefas],
-    })
-  }
+    });
+  };
 
   render() {
     const { novaTarefa, tarefas } = this.state;
     return (
       <div className="main">
         <h1>Lista de Tarefas</h1>
-        <form onSubmit={this.handleSubmit} action="#" className="xform">
-          <input
-            onChange={this.handleChange}
-            type="text"
-            value={novaTarefa}
-          />
-          <button type="submit"><FaPlus /></button>
-        </form>
 
-        <ul className="tarefas">
-          {tarefas.map((tarefa, index) => (
-            <li key={tarefa}>
-              {tarefa}
-              <span>
-                <FaEdit
-                  className="edit"
-                  onClick={(e) => this.handleEdit(e, index)}
-                />
-                <FaWindowClose
-                  onClick={(e) => this.handleDelete(e, index)}
-                  className="delete"
-                />
-              </span>
-            </li>
-          ))}
-        </ul>
+        <Form
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          novaTarefa={novaTarefa}
+        />
+
+        <Tarefas
+          tarefas={tarefas}
+          handleEdit={this.handleEdit}
+          handleDelete={this.handleDelete}
+        />
       </div>
     );
   }
